9febf81556a91400e038b1cafbbc361dec62a8dd
remixxx31 <remiperray@gmail.com>
2021-09-26 00:56:01 +0200

    boucle score aléatoire et calcul du round

---
+++ TEST/test.js
@@ -0,0 +1,37 @@
+// fonction qui me renvoit un nb aléatoire sur les bornes précisées
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+
+function result_Dice(result){
+  let round = 0;
+  // result = null;
+  do{
+    result= getRandomInt(1,7);
+    if(result != 1){
+    round += result;
+    console.log(
+    `Votre lancer de dé est de : ${result},
+    votre round est de: ${round},
+    continuez ou envoyer vos points sur le score global !
+    
+    `);
+  }else{
+      console.log(
+        `Votre lancer de dé est de : ${result},
+        vous perdez les points du round et c'est à votre adversaire!
+      
+        `);
+    }
+  } while (result != 1)
+  round = 0;
+}
+
+
+
+result_Dice();
+
+
