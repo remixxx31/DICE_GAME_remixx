66893137b755b06f3074d60369b7ee650305e586
remixxx31 <remiperray@gmail.com>
2021-09-26 16:49:25 +0200

    ALgo sous forme de boucle affichage résultat dé et round

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min
 }
+function result_Dice(result){
+  let round = 0;
+  // result = null;
+  do{
+    result= getRandomInt(1,7);
+    if(result != 1){
+    round += result;
+    console.log(
+    `Votre lancer de dé est de : ${result},
+      Votre round est de: ${round},
+      continuez ou envoyer vos points sur le score global !
+    
+    `);
+  }else{
+      console.log(
+        `Votre lancer de dé est de : ${result},
+        vous perdez les points du round et c'est à votre adversaire!
+      
+        `);
+    }
+  } while (result != 1)
+  round = 0;
+}
+
+
+
+result_Dice();
