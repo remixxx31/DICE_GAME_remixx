2e988d9dfb1731faddecf3c650568cfc7357bfb6
remixxx31 <remiperray@gmail.com>
2021-09-27 17:08:39 +0200

    Mise en place des différentes fonctions principales

--- app.js
+++ app.js
@@ -1,65 +1,103 @@
-// fonction qui me renvoit un nb aléatoire sur les bornes précisées
-function get_random_int(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min
-}
-
+//définition des classes
 let game;
 
 let player1;
 
 let player2;
 
+// initialisation des classes
 function game_start() {
+  //initialisation des différentes classes
   game = {
-      turn: get_random_int(0, 2),
-      win: null,
+    turn: get_random_int(0, 2),
+    win: null,
   }
-
+  
   player1 = {
-      round: null,
-      global: null,
-      result: null,
+    round: null,
+    global: null,
+    result: null,
   }
-
+  
   player2 = {
-      round: null,
-      global: null,
-      result: null,
+    round: null,
+    global: null,
+    result: null,
   }
 }
 
-// let round=null ;
+function add_listener() {
+  const diceRoll = document.querySelector("#dice-roll");
+  diceRoll.addEventListener("click", roll_dice);
+}
 
-// function result_Dice(result){
-//   // result = null;
-//   result= getRandomInt(1,7);
-//   if(result != 1){
-//     round+=result;
-    
-//     console.log(
-//       `Votre lancer de dé est de : ${result},
-//       Votre round est de: ${round},
-//       continuez ou envoyer vos points sur le score global !
-      
-//       `);
-//       // round += result;
-//       // console.log(round);
-      
-//     }else{
-//       round = 0;
-//       console.log(
-//         `Votre lancer de dé est de : 1,
-//         vous perdez les points du round et c'est à votre adversaire!
-        
-//         `);
-//       }
-      
-//     }
-    
-// let getRound =(result_Dice())+round;
-// console.log(getRound);   
-result_Dice();
-result_Dice();
-result_Dice();
+// fonction qui me renvoit un nb aléatoire entre les bornes précisées
+function get_random_int(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+//fonction du lancer de dé
+function roll_dice () {
+  console.log("ok")
+  result = get_random_int(1,7);
+  if(result != 1){
+      notOne();
+  }else{
+      isOne();
+  }
+}
+
+//fonction résultat du dé à 1
+function is_one () {
+  if(game.turn === 0) {
+      player1.round = 0;
+      game.turn = 1;
+  }else{
+      player2.round = 0;
+      game.turn = 0;
+  }
+}
+
+//fonction résultat différent de 1
+function not_one() {
+  if(game.turn === 0) {
+      player1.round = round+=result;
+  }else{
+      player2.round = round+=result;
+  }
+}
+
+//fonction envoi du score sur score global
+function hold () {
+  if(game.turn === 0) {
+      player1.global+=player1.round;
+      if(is_victory(player1.global)){
+          game.win = 0;
+      }else{
+          game.turn = 1;
+      }
+  }else{
+      player2.global+=player2.round;
+      if(is_victory(player2.global)){
+          game.win = 1;
+      }else{
+          game.turn = 0;
+      }
+  }
+}
+
+
+//fonction qui évalue si il ya un vainqueur
+function is_victory(global) {
+  if(global >= 100){
+      return true;
+  }else{
+      return false;
+  }
+}
+
+game_start();  
+   
+
